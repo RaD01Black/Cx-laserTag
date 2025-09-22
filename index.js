c1cdec66497d7b1334e32aa75aafba81b6fab295
Alereza_RaDBlaCK01 <aliireza01ahmadi@gmail.com>
2025-09-22 23:19:31 +0200

    #1

---
+++ index.js
@@ -0,0 +1,14 @@
+const pages = ["home", "about", "contact"];
+
+function showPage(id) {
+  for (let p of pages) {
+    document.getElementById(p).style.display = (p === id ? "block" : "none");
+  }
+}
+
+document.getElementById("nav-home").onclick = () => showPage("home");
+document.getElementById("nav-about").onclick = () => showPage("about");
+document.getElementById("nav-contact").onclick = () => showPage("contact");
+
+// ✅ Start with home page visible
+showPage("home");
