2cfd05ea88ad8915e965b50ad2b803e29d64794d
Alereza_RaDBlaCK01 <aliireza01ahmadi@gmail.com>
2025-09-26 01:54:14 +0200

    Fixing this shit

--- index.js
+++ index.js
@@ -6,27 +6,26 @@ function pageHandler(page) {
   });
 }
 
-const mapDivs = document.querySelectorAll(".MapSelectDiv");
+  const mapDivs = document.querySelectorAll(".MapSelectDiv");
 
-mapDivs.forEach((div, index) => {
-  const green = div.querySelector(".GreenSelectMap");
-  const red = div.querySelector(".RedSelectMap");
-
-  if (index === 0) {
-    green.classList.add("active");
-  } else {
-    red.classList.add("active");
-  }
-});
-
-mapDivs.forEach(div => {
-  div.addEventListener("click", () => {
-    mapDivs.forEach(d => {
-      d.querySelector(".GreenSelectMap").classList.remove("active");
-      d.querySelector(".RedSelectMap").classList.add("active");
-    });
-
-    div.querySelector(".GreenSelectMap").classList.add("active");
-    div.querySelector(".RedSelectMap").classList.remove("active");
+  mapDivs.forEach((div, i) => {
+    const green = div.querySelector(".GreenSelectMap");
+    const red = div.querySelector(".RedSelectMap");
+    if (i === 0) {
+      green.classList.add("active");
+    } else {
+      red.classList.add("active");
+    }
   });
-});
+
+  mapDivs.forEach(div => {
+    div.addEventListener("click", () => {
+      mapDivs.forEach(d => {
+        d.querySelector(".GreenSelectMap").classList.remove("active");
+        d.querySelector(".RedSelectMap").classList.add("active");
+      });
+
+      div.querySelector(".GreenSelectMap").classList.add("active");
+      div.querySelector(".RedSelectMap").classList.remove("active");
+    });
+  });
